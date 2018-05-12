81da9578c2ec7745bc74eda16d62a8e8cefb7b0c
lexuanlong2941996 <lexuanlong2941996@gmail.com>
2018-05-12 17:41:44 +0700

    FE

---
+++ QuanLyBanSach-dbf/QuanLyBanSach-dbf/QuanLyBanSach/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyBanSach.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookStore_RecoveredEntities1 db = new BookStore_RecoveredEntities1();
+        public ActionResult Index()
+        {
+            ViewData["Authors"] = db.Authors.ToList();
+            ViewData["Categories"] = db.Authors.ToList();
+            return View();
+        }
+
+        
+    }
+}
