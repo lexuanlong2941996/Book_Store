6a30c0f8daef9b536ed5d68e34afa4bfd2b8278f
phamnutuyetan <14520010@gm.uit.edu.vn>
2018-05-14 12:08:57 +0700

    Bỏ bundle config, đổi cấu trúc directory, bỏ jquery thừa

--- QuanLyBanSach-dbf/QuanLyBanSach-dbf/QuanLyBanSach/Global.asax.cs
+++ QuanLyBanSach-dbf/QuanLyBanSach-dbf/QuanLyBanSach/Global.asax.cs
@@ -15,7 +15,6 @@ namespace QuanLyBanSach
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     
     }
